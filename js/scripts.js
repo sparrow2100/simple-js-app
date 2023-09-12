27798076f2cb031a30e9f0f5749dae8d3f43a8ad
sparrow2100 <111162787+sparrow2100@users.noreply.github.com>
2023-09-12 12:36:03 -0400

    Create pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert("hello world!");
-
-let favouriteFood = "sushi";
-document.write(favouriteFood);
+let pokemonList = [
+  {
+    name: "Charizard",
+    height: 1.7,
+    type: ["fire", "flying"],
+  },
+  {
+    name: "Ivysaur",
+    height: 1,
+    type: ["grass", "poison"],
+  },
+  {
+    name: "Pidgeot",
+    height: 1.5,
+    type: ["flying", "normal"],
+  },
+];
