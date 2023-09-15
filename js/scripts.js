aa739e23a7d908417800ca194b37d8d3c703ed70
sparrow2100 <111162787+sparrow2100@users.noreply.github.com>
2023-09-15 19:48:47 -0400

    change for loop to forEach(), wrap pokemon array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,57 @@
-let pokemonList = [
-  {
-    name: "Charizard",
-    height: 1.7,
-    type: ["fire", "flying"],
-  },
-  {
-    name: "Ivysaur",
-    height: 1,
-    type: ["grass", "poison"],
-  },
-  {
-    name: "Pidgeot",
-    height: 1.5,
-    type: ["flying", "normal"],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Charizard",
+      height: 1.7,
+      type: ["fire", "flying"],
+    },
+    {
+      name: "Ivysaur",
+      height: 1,
+      type: ["grass", "poison"],
+    },
+    {
+      name: "Pidgeot",
+      height: 1.5,
+      type: ["flying", "normal"],
+    },
+  ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.5) {
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  return {
+    getAll: getAll,
+
+    add: add,
+  };
+})();
+
+let pokemonResult = pokemonRepository.getAll();
+
+pokemonResult.forEach((pokemon) => {
+  if (pokemon.height > 1.5) {
     document.write(
-      `Name: ${pokemonList[i].name} (height: ${pokemonList[i].height}--wow, that's big!) `
+      `Name: ${pokemon.name} (height: ${pokemon.height}--wow, that's big!) `
     );
   } else {
-    document.write(
-      `Name: ${pokemonList[i].name} (height: ${pokemonList[i].height}) `
-    );
+    document.write(`Name: ${pokemon.name} (height: ${pokemon.height}) `);
   }
-}
+});
+
+// for (let i = 0; i < pokemonList.length; i++) {
+//   if (pokemonList[i].height > 1.5) {
+//     document.write(
+//       `Name: ${pokemonList[i].name} (height: ${pokemonList[i].height}--wow, that's big!) `
+//     );
+//   } else {
+//     document.write(
+//       `Name: ${pokemonList[i].name} (height: ${pokemonList[i].height}) `
+//     );
+//   }
+// }
