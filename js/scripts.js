e6ddb1f0b612407bdb81cbb858eb454bd40c96fd
sparrow2100 <111162787+sparrow2100@users.noreply.github.com>
2023-10-11 22:25:53 -0400

    Make search bar functional

--- js/scripts.js
+++ js/scripts.js
@@ -171,6 +171,21 @@ let pokemonRepository = (function () {
   };
 })();
 
+//add search feature
+let searchBar = document.getElementById("search");
+searchBar.addEventListener("input", (e) => {
+  let searchValue = e.target.value.toLowerCase();
+  console.log(searchValue);
+  document
+    .querySelectorAll(".pokemon-list .list-group-item")
+    .forEach((listItem) => {
+      listItem.classList.toggle(
+        "hide",
+        !listItem.textContent.toLowerCase().includes(searchValue)
+      );
+    });
+});
+
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach((pokemon) => {
     pokemonRepository.addListItem(pokemon);
